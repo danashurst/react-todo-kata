498eeb6f23d117acaf3909f0fe228f45ead4e71d
Owen Ashurst <owen.ashurst@betfred.com>
2019-10-09 15:04:58 +0100

    Added ability for the enter key to add a todo item. Added error message for if the box is left empty and a todo is attempting to be added.

--- src/components/new-item-form/new-item-form.js
+++ src/components/new-item-form/new-item-form.js
@@ -5,35 +5,86 @@ import './new-item-form.scss';
 
 const NewItemForm = () => {
     const [,todoListItemsDispatch] = useContext(ToDoListContext);
-    const [newItemState, newItemDispatch] = useState('');
+    
+    const [newItemState, setNewItemState] = useState({
+        item: '',
+        error: false,
+        errorMessage: ''
+    });
 
     const onItemChange = (e) => {
-        newItemDispatch(e.target.value);
+        setNewItemState({
+            ...newItemState,
+            item: e.target.value,
+            error: false,
+            errorMessage: ''
+        });
     };
 
     const onItemSubmit = () => {
+        if (newItemState.item.length > 0) {
+            submitItemAndClearInputBox();
+        } else {
+            showError();
+        }
+    };
+
+    const onKeyDown = (e) => {
+        if (e.key !== 'Enter') return;
+
+        if (newItemState.item.length > 0) {
+            submitItemAndClearInputBox();
+        } else {
+            showError();
+        }
+    };
+
+    const submitItemAndClearInputBox = () => {
         const date = new Date();
 
         todoListItemsDispatch({
             type: TODO_ITEM_ADD,
             id: date.getTime(),
-            message: newItemState,
+            message: newItemState.item,
             created: date
         });
 
-        newItemDispatch('');
+        setNewItemState({
+            ...newItemState,
+            item: '',
+            error: false,
+            errorMessage: ''
+        });
+    };
+
+    const showError = () => {
+        setNewItemState({
+            ...newItemState,
+            error: true,
+            errorMessage: 'Box cannot be left empty'
+        });
     };
 
     return (
-        <div className="form-container">
+        <div className='form-container'>
             <input 
-                className="form-input" 
-                value={newItemState} 
-                onChange={onItemChange} />
+                className='form-input'
+                value={newItemState.item} 
+                onChange={onItemChange}
+                onKeyDown={onKeyDown} />
 
             <button 
-                className="form-button" 
+                className='form-button'
                 onClick={onItemSubmit}>Submit</button>
+
+            {
+                newItemState.error 
+                ? 
+                    <div className='form-error-text'>{newItemState.errorMessage}</div>
+                :
+                   ''
+            }
+            
         </div>
     )
 };
